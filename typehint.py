ed2e961888483cd333f20d4fb03686cd93b26d08
Leonardo Salin <domafras@gmail.com>
2025-03-21 21:16:04 -0300

    adiciona exemplo 2 type hint

--- typehint.py
+++ typehint.py
@@ -9,3 +9,11 @@ idade: int = 30
 altura: float = 1.75
 nome: str = "Alice"
 estudante: bool = True
+
+# Type Hint é apenas uma dica de tipagem.
+"""
+idade: int = input("Digite sua idade")
+print(type(idade))
+
+<class 'str'>
+"""
