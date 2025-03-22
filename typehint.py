fd47839d58652c5d6fb69f7966cf6a20e49d8c81
Leonardo Salin <domafras@gmail.com>
2025-03-21 21:09:54 -0300

    adiciona exemplo de type hint

---
+++ typehint.py
@@ -0,0 +1,11 @@
+# Sem Type Hint
+idade = 30
+altura = 1.75
+nome = "Alice"
+is_estudante = True
+
+# Com Type Hint
+idade: int = 30
+altura: float = 1.75
+nome: str = "Alice"
+estudante: bool = True
